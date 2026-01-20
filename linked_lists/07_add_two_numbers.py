c73d992f418639bd45a6a622bc4e964e6a64cb5d
murali krishna-AI <muralitirumanadham@gmail.com>
2026-01-20 11:46:34 +0530

    feat: add two numbers represented by linked lists

--- linked_lists/07_add_two_numbers.py
+++ linked_lists/07_add_two_numbers.py
@@ -1,1 +1,68 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        dummy = ListNode(0)
+        current = dummy
+        carry = 0
+
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+
+            total = val1 + val2 + carry
+            carry = total // 10
+            digit = total % 10
+
+            current.next = ListNode(digit)
+            current = current.next
+
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+
+        return dummy.next
+
+
+def create_linked_list(values):
+    if not values:
+        return None
+
+    head = ListNode(values[0])
+    current = head
+
+    for val in values[1:]:
+        current.next = ListNode(val)
+        current = current.next
+
+    return head
+
+
+def print_linked_list(head):
+    current = head
+    while current:
+        print(current.val, end=" -> ")
+        current = current.next
+    print("None")
+
+
+l1 = create_linked_list([2, 4, 3])
+l2 = create_linked_list([5, 6, 4])
+
+print("List 1:")
+print_linked_list(l1)
+
+print("\nList 2:")
+print_linked_list(l2)
+
+solution = Solution()
+result = solution.addTwoNumbers(l1, l2)
+
+print("\nResult:")
+print_linked_list(result)
 
