4ed7d8938c0bd8fb728d697fe789837e9f6178c4
murali krishna-AI <muralitirumanadham@gmail.com>
2026-01-20 08:08:12 +0530

    feat: merge two sorted linked lists

--- linked_lists/03_merge_two_sorted_lists.py
+++ linked_lists/03_merge_two_sorted_lists.py
@@ -1,1 +1,65 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        dummy = ListNode(0)
+        current = dummy
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+            current = current.next
+
+        if list1:
+            current.next = list1
+        if list2:
+            current.next = list2
+
+        return dummy.next
+
+
+def create_linked_list(values):
+    if not values:
+        return None
+
+    head = ListNode(values[0])
+    current = head
+
+    for val in values[1:]:
+        current.next = ListNode(val)
+        current = current.next
+
+    return head
+
+
+def print_linked_list(head):
+    current = head
+    while current:
+        print(current.val, end=" -> ")
+        current = current.next
+    print("None")
+
+
+list1 = create_linked_list([1, 3, 5])
+list2 = create_linked_list([2, 4, 6])
+
+print("List 1:")
+print_linked_list(list1)
+
+print("\nList 2:")
+print_linked_list(list2)
+
+solution = Solution()
+merged = solution.mergeTwoLists(list1, list2)
+
+print("\nMerged List:")
+print_linked_list(merged)
 
