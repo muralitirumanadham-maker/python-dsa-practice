c7d833927f103dfa0b20f378690c5d4065247c92
murali krishna-AI <muralitirumanadham@gmail.com>
2026-01-20 08:20:05 +0530

    feat: detect cycle in linked list using floyd algorithm

--- linked_lists/05_linked_list_cycle.py
+++ linked_lists/05_linked_list_cycle.py
@@ -1,1 +1,62 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def hasCycle(self, head):
+        if head is None:
+            return False
+
+        slow = head
+        fast = head
+
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+
+        return False
+
+
+def create_linked_list(values):
+    if not values:
+        return None
+
+    head = ListNode(values[0])
+    current = head
+
+    for val in values[1:]:
+        current.next = ListNode(val)
+        current = current.next
+
+    return head
+
+
+def create_cycle(head, pos):
+    if pos == -1:
+        return head
+
+    cycle_node = None
+    current = head
+    index = 0
+
+    while current.next:
+        if index == pos:
+            cycle_node = current
+        current = current.next
+        index += 1
+
+    current.next = cycle_node
+    return head
+
+
+values = [1, 2, 3, 4, 5]
+head = create_linked_list(values)
+head = create_cycle(head, 2)
+
+solution = Solution()
+print(solution.hasCycle(head))
 
