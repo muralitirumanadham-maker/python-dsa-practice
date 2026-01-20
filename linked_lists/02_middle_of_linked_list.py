ae20f17f7e788aade3804404c198968a1d1b6317
murali krishna-AI <muralitirumanadham@gmail.com>
2026-01-20 08:01:15 +0530

    feat: find middle of linked list using slow-fast pointers

--- linked_lists/02_middle_of_linked_list.py
+++ linked_lists/02_middle_of_linked_list.py
@@ -1,1 +1,52 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def middleNode(self, head):
+        slow = head
+        fast = head
+
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
+
+
+def create_linked_list(values):
+    if not values:
+        return None
+
+    head = ListNode(values[0])
+    current = head
+
+    for val in values[1:]:
+        current.next = ListNode(val)
+        current = current.next
+
+    return head
+
+
+def print_linked_list(head):
+    current = head
+    while current:
+        print(current.val, end=" -> ")
+        current = current.next
+    print("None")
+
+
+values = [1, 2, 3, 4, 5]
+head = create_linked_list(values)
+
+print("Linked List:")
+print_linked_list(head)
+
+solution = Solution()
+middle = solution.middleNode(head)
+
+print("\nMiddle Node:")
+print_linked_list(middle)
 
