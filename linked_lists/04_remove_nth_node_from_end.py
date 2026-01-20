bcf164c80c53b4698f7a03dc785f20907b676f70
murali krishna-AI <muralitirumanadham@gmail.com>
2026-01-20 08:13:46 +0530

    feat: remove nth node from end using two pointers

--- linked_lists/04_remove_nth_node_from_end.py
+++ linked_lists/04_remove_nth_node_from_end.py
@@ -1,1 +1,57 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        dummy = ListNode(0)
+        dummy.next = head
+        slow = dummy
+        fast = dummy
+
+        for _ in range(n + 1):
+            fast = fast.next
+
+        while fast:
+            fast = fast.next
+            slow = slow.next
+
+        slow.next = slow.next.next
+        return dummy.next
+
+
+def create_linked_list(values):
+    if not values:
+        return None
+
+    head = ListNode(values[0])
+    current = head
+
+    for val in values[1:]:
+        current.next = ListNode(val)
+        current = current.next
+
+    return head
+
+
+def print_linked_list(head):
+    current = head
+    while current:
+        print(current.val, end=" -> ")
+        current = current.next
+    print("None")
+
+
+values = [1, 2, 3, 4, 5]
+head = create_linked_list(values)
+
+print("Original List:")
+print_linked_list(head)
+
+solution = Solution()
+new_head = solution.removeNthFromEnd(head, 2)
+
+print("\nAfter Removing 2nd Node From End
 
