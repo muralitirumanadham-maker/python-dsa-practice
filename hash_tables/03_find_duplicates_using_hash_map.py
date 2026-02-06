8400daf3d6d9a81b34dd7529e8ecdf761a8e556a
murali krishna-AI <muralitirumanadham@gmail.com>
2026-02-06 21:14:23 +0530

    feat: find duplicate elements using hash map

---
+++ hash_tables/03_find_duplicates_using_hash_map.py
@@ -0,0 +1,21 @@
+def find_duplicates(nums):
+    my_dict={}
+    duplicates=[]
+    for i in nums:
+        if i in my_dict:
+            if i not in duplicates:
+                duplicates.append(i)
+        else:
+            my_dict[i]=True
+    return duplicates
+
+
+
+
+print ( find_duplicates([1, 2, 3, 4, 5]) )
+print ( find_duplicates([1, 1, 2, 2, 3]) )
+print ( find_duplicates([1, 1, 1, 1, 1]) )
+print ( find_duplicates([1, 2, 3, 3, 3, 4, 4, 5]) )
+print ( find_duplicates([1, 1, 2, 2, 2, 3, 3, 3, 3]) )
+print ( find_duplicates([1, 1, 1, 2, 2, 2, 3, 3, 3, 3]) )
+print ( find_duplicates([]) )
