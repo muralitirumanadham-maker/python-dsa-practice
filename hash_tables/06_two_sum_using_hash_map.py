a9ac8dcf0f4aa34d1ce560ddb0793d420306fb0a
murali krishna-AI <muralitirumanadham@gmail.com>
2026-02-07 08:24:22 +0530

    feat: solve two sum using hash map for optimal lookup

---
+++ hash_tables/06_two_sum_using_hash_map.py
@@ -0,0 +1,18 @@
+def two_sum(nums,target):
+    seen={}
+    for i in range(len(nums)):
+        current=nums[i]
+        needed=target-current
+        if needed in seen:
+            return [seen[needed],i]
+        seen[current]=i
+    return []
+
+print(two_sum([5, 1, 7, 2, 9, 3], 10))  
+print(two_sum([4, 2, 11, 7, 6, 3], 9))  
+print(two_sum([10, 15, 5, 2, 8, 1, 7], 12))  
+print(two_sum([1, 3, 5, 7, 9], 10))  
+print ( two_sum([1, 2, 3, 4, 5], 10) )
+print ( two_sum([1, 2, 3, 4, 5], 7) )
+print ( two_sum([1, 2, 3, 4, 5], 3) )
+print ( two_sum([], 0) )
