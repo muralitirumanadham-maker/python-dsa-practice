0bde7489b4ec9947446e233d73793cdc5936abba
murali krishna-AI <muralitirumanadham@gmail.com>
2026-02-18 15:59:02 +0530

    feat: implement number of islands using dfs

---
+++ Graphs/17_number_of_islands.py
@@ -0,0 +1,29 @@
+def no_of_islands(grid):
+    if not grid:
+        return 0
+    rows,cols=len(grid),len(grid[0])
+    islands=0
+    def dfs(r,c):
+        if r<0 or r>=rows or c<0 or c>=cols or grid[r][c]=="0":
+            return
+        grid[r][c]="0"
+        dfs(r+1,c)
+        dfs(r-1,c)
+        dfs(r,c+1)
+        dfs(r,c-1)
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c]=="1":
+                islands+=1
+                dfs(r,c)
+    return islands
+
+grid = [
+    ["1","1","0","0","0"],
+    ["1","1","0","0","0"],
+    ["0","0","1","0","0"],
+    ["0","0","0","1","1"]
+]
+
+# ---------- OUTPUT ----------
+print("Number of Islands:", no_of_islands(grid))
